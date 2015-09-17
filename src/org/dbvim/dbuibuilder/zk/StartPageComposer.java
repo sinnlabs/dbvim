152d42520b0f1465bc50f151f4bd351268d6027b
Peter Liverovsky <nsa-azon@yandex.ru>
2015-09-17 17:18:37 +0300

    Add default start page.

---
+++ src/org/dbvim/dbuibuilder/zk/StartPageComposer.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package org.dbvim.dbuibuilder.zk;
+
+import org.dbvim.dbuibuilder.config.ConfigLoader;
+import org.dbvim.dbuibuilder.model.Form;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.select.SelectorComposer;
+import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zul.A;
+import org.zkoss.zul.Cell;
+import org.zkoss.zul.Grid;
+import org.zkoss.zul.Label;
+import org.zkoss.zul.Row;
+
+/**
+ * @author peter.liverovsky
+ *
+ */
+public class StartPageComposer extends SelectorComposer<Component> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6638008020345812736L;
+	
+	@Wire
+	Grid gridForms;
+	
+	@Override
+	public void doAfterCompose(Component comp) throws Exception {
+		super.doAfterCompose(comp);
+		
+		// Fill grid with forms
+		gridForms.getRows().getChildren().clear();
+		
+		for(Form form : ConfigLoader.getInstance().getForms().queryForAll()) {
+			Row row = new Row();
+			Cell nameCell = new Cell();
+			Cell linkCell = new Cell();
+			nameCell.appendChild(new Label(form.getName()));
+			A link = new A();
+			link.setTarget("_blank");
+			link.setHref("/data/" + form.getName());
+			link.setLabel(form.getName());
+			linkCell.appendChild(link);
+			row.appendChild(nameCell);
+			row.appendChild(linkCell);
+			gridForms.getRows().appendChild(row);
+		}
+	}
+
+}
