d920b868614e51ccc628fc517f01c79e7d525eee
Peter Liverovsky <nsa-azon@yandex.ru>
2016-02-16 23:30:53 +0300

    Add javadoc

--- src/org/sinnlabs/dbvim/db/model/IDBField.java
+++ src/org/sinnlabs/dbvim/db/model/IDBField.java
@@ -4,24 +4,54 @@
 package org.sinnlabs.dbvim.db.model;
 
 /**
+ * Interface that represents database field/column properties
  * @author peter.liverovsky
  *
  */
 public interface IDBField {
 	
+	/**
+	 * Returns db field name
+	 */
 	public String getName();
 	
+	/**
+	 * Returns string that represents db type name
+	 */
 	public String getDBTypeName();
 	
+	/**
+	 * Returns db table name
+	 */
 	public String getTableName();
 	
+	/**
+	 * Returns db catalog
+	 */
 	public String getCatalogName();
 	
+	/**
+	 * Returns qualified field name:
+	 * [CatalogName].TableName.DBFieldName
+	 * @return
+	 */
 	public String getFullName();
 	
+	/**
+	 * Returns java.Sql type
+	 */
 	public int getDBType();
 	
+	/**
+	 * Indicates that field value is automatically generated.
+	 * For example: MSSQL Identity fields
+	 * @return true if field is auto generetad, otherwise false
+	 */
 	public boolean isGenerated();
 	
+	/**
+	 * Indicates that field can be set to null
+	 * @return true if field can be set to null, otherwise false
+	 */
 	public boolean isNullable();
 }
